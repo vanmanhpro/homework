20da507dabe61225dc9ea5f0da0a26a0be7e74eb
vanmanhpro <vanmanhpbc1998@gmail.com>
2017-09-04 12:04:17 +0700

    Homeworked 4/9

--- statistic.js
+++ statistic.js
@@ -16,8 +16,29 @@ Router.get('/', (req, res) => {
 		});
 })
 
+
 Router.post('/', (req, res) => {
 
+	if (req.body.home === '') {
+		res.redirect('/');
+	} else if (req.body.contribute === '') {
+		res.redirect('/contribute');
+	};
+})
+
+Router.get('/:id', (req, res) => {
+	var questionNumber = req.params.id;
+	var questionStatistic = fileInteraction.getQuestionStatisticObject(questionNumber);
+
+	res.render('statistic', {
+			question : questionStatistic.question,
+			yesVote : questionStatistic.yesVote,
+			noVote : questionStatistic.noVote
+		});	
+})
+
+
+Router.post('/:id', (req, res) => {
 	if (req.body.home === '') {
 		res.redirect('/');
 	} else if (req.body.contribute === '') {
